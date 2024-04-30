717e420033924b7f0051a48023eebc6098ce808b
EmersonLaranja <emersonlaranja@gmail.com>
2024-04-30 15:20:47 -0300

    1.5

--- src/adapters/controllers/task/addTask.spec.ts
+++ src/adapters/controllers/task/addTask.spec.ts
@@ -1,6 +1,7 @@
 import { Task } from "../../../entities/task";
 import { AddTask, AddTaskModel } from "../../../usecases";
 import { HttpRequest, Validation } from "../../interfaces";
+import { serverError } from "../../presentations/api/httpResponses/httpResponses";
 import { AddTaskController } from "./addTask";
 
 const makeAddTask = (): AddTask => {
@@ -65,5 +66,24 @@ describe("AddTask Controller", () => {
       description: "any_description",
       date: "30/06/2024",
     });
+  });
+
+  test("Deve retornar 500 se AddTask lançar uma exceção", async () => {
+    const { sut, addTaskStub } = makeSut();
+    jest
+      .spyOn(addTaskStub, "add")
+      .mockImplementationOnce(async () => Promise.reject(new Error()));
+    const httpResponse = await sut.handle(makeFakeRequest());
+
+    expect(httpResponse).toEqual(serverError(new Error()));
+  });
+  test("Deve chamar Validation com valores corretos", async () => {
+    const { sut, validationStub } = makeSut();
+
+    const validateSpy = jest.spyOn(validationStub, "validate");
+    const httpRequest = makeFakeRequest();
+    await sut.handle(httpRequest);
+
+    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
   });
 });
